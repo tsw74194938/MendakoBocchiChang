7b841aff419ccc283a3028ac5622e6bce1351873
tsw74194938 <tannestmanse0d@gmail.com>
2024-03-31 22:37:43 +0900

    :sparkles: めんだこぼっちを回転させる

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
-import { Application } from 'pixi.js';
+import { Application, Assets, Sprite } from 'pixi.js';
 
 const app = new Application();
 
 async function init() {
   await app.init({
-    width: 400,
-    height: 300,
-    backgroundColor: 0x000000,
+    width: 600,
+    height: 600,
+    backgroundColor: 0xabcfb5,
   });
   document.body.appendChild(app.canvas);
+
+  const texture = await Assets.load('./img/bocchi-chang.png');
+  const sprite = Sprite.from(texture);
+  sprite.anchor.set(0.5, 0.5);
+  sprite.x = app.screen.width / 2;
+  sprite.y = app.screen.height / 2;
+  app.stage.addChild(sprite);
+
+  app.ticker.add((time) => {
+    sprite.rotation -= 0.05 * time.deltaTime;
+  });
 }
 
 init();
