99641e617398f804a2dafc35971758e3ee8e88c5
tsw74194938 <tannestmanse0d@gmail.com>
2024-04-02 00:39:17 +0900

    :sparkles: ぼっちちゃんタッチ時にサウンドを鳴らすことができる

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { Application, Assets, Sprite } from 'pixi.js';
+import { Sound } from '@pixi/sound';
 import { config as designConfig, resizeIfNeeded } from './resize';
 import bocchiChang from '/img/bocchi-chang.png';
+import touchSound from '/audio/bocchi-touch.mp3';
 
 const app = new Application();
 
@@ -23,6 +25,16 @@ async function init() {
   bocchi.x = app.screen.width / app.stage.scale.x / 2;
   bocchi.y = app.screen.height / app.stage.scale.y / 2;
   app.stage.addChild(bocchi);
+
+  bocchi.interactive = true;
+  bocchi.onclick = onTouch;
+  bocchi.ontouchstart = onTouch;
+}
+
+function onTouch() {
+  const sound = Sound.from(touchSound);
+  sound.resume();
+  sound.play();
 }
 
 init();
